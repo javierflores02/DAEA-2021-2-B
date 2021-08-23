8e5283b7377622dff9968192aa146752d533c2ab
Javier Flores <javier.flores@tecsup.edu.pe>
2021-08-23 16:28:32 -0500

    Laboratorio 01 - Parte 1

---
+++ lab01/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuál es tu nombre?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
