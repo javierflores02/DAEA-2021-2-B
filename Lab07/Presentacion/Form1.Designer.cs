7d54b98f59688afd57547631bd6e1319f29911b9
Javier Flores <javier.flores@tecsup.edu.pe>
2021-10-15 11:59:02 -0500

    Laboratorio 7 - Ejercicio 4: Desarrollo de la capa de presentación

--- Lab07/Presentacion/Form1.Designer.cs
+++ Lab07/Presentacion/Form1.Designer.cs
@@ -29,13 +29,38 @@ namespace Presentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgDatos
+            // 
+            this.dgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDatos.Location = new System.Drawing.Point(9, 11);
+            this.dgDatos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgDatos.Name = "dgDatos";
+            this.dgDatos.RowHeadersWidth = 51;
+            this.dgDatos.RowTemplate.Height = 24;
+            this.dgDatos.Size = new System.Drawing.Size(495, 396);
+            this.dgDatos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(513, 417);
+            this.Controls.Add(this.dgDatos);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgDatos;
     }
 }
-
