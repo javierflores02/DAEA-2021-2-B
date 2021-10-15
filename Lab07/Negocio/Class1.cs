982e42094a43ddee83e0615bad49587346e83873
Javier Flores <javier.flores@tecsup.edu.pe>
2021-10-15 11:49:12 -0500

    Laboratorio 7 - Ejercicio 2: Desarrollo de la capa de datos

--- Lab07/Negocio/Class1.cs
+++ Lab07/Negocio/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos;
+using System.Data;
 
 namespace Negocio
 {
-    public class Class1
+    public class clsNegPerson
     {
+        clsDAOPerson daoPerson = new clsDAOPerson();
+
+        public DataTable GetAll()
+        {
+            return daoPerson.GetAll();
+        }
     }
 }
