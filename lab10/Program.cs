846cf20d9c6b2833330e5e575bfd375489a16699
Javier Flores <javier.flores@tecsup.edu.pe>
2021-10-21 22:29:36 -0500

    Laboratorio 10 - Punto 2: j (5 Consultas)

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -26,8 +26,50 @@ namespace lab10
                 Console.WriteLine("ID={0} \t Price={1} \t Name={2} ", prod.ProductID, prod.UnitPrice, prod.ProductName);
             }
 
+            //Consulta para obtener los productos cuyo nombre incluye la palabra queso
+            var query_queso = from p in context.Products
+                              where SqlMethods.Like(p.ProductName, "%Queso%")
+                              select p;
+
+            //Consulta para obtener los productos cuya presentacion sea paquetes (pkg o pkgs)
+            var query_paquetes = from p in context.Products
+                                 where SqlMethods.Like(p.QuantityPerUnit, "%pkg%")
+                                 select p;
+
+            //Consulta para obtener los productos que empiezan con la letra a
+            var query_a = from p in context.Products
+                          where SqlMethods.Like(p.ProductName, "A%")
+                          select p;
+
+
+            //Consulta para obtener los productos sin  stock
+            var query_sin_stock = from p in context.Products
+                                  where p.UnitsInStock == 0
+                                  select p;
+
+            //Consulta para obtener los productos descontonuados
+            var query_descontonuados = from p in context.Products
+                                       where p.Discontinued == true
+                                       select p;
+
+            //ejecucion de la consulta
+            readQuery(query_queso, "Consulta para obtener los productos cuyo nombre incluye la palabra queso");
+            readQuery(query_paquetes, "Consulta para obtener los productos cuya presentacion sea paquetes (pkg o pkgs)");
+            readQuery(query_a, "Consulta para obtener los productos que empiezan con la letra a");
+            readQuery(query_sin_stock, "Consulta para obtener los productos sin  stock");
+            readQuery(query_descontonuados, "Consulta para obtener los productos descontonuados");
+
             Console.ReadKey();
 
+        }
+
+        private static void readQuery(IQueryable<dynamic> query, String msg)
+        {
+            Console.WriteLine("\n" + msg);
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Name={1} ", prod.ProductID, prod.ProductName);
+            }
         }
     }
 }
