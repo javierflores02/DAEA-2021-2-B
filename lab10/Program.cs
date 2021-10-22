53bd935f1fe0face52ad5c1cebd4ac9595c63ee8
Javier Flores <javier.flores@tecsup.edu.pe>
2021-10-21 21:51:56 -0500

    Laboratorio 10 - Punto 2: h (Listado de productos con categoría: Beverages)

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -10,8 +10,24 @@ namespace lab10
     class Program
     {
         static void Main(string[] args)
-        { 
-        }
+        {
+            //Origen de datos
+            NorthwndDataContext context = new NorthwndDataContext();
 
+
+            //Creacion de la consulta
+            var query = from p in context.Products
+                        where p.Categories.CategoryName == "Beverages"
+                        select p;
+
+            //Ejecución de la consulta
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+            }
+
+            Console.ReadKey();
+
+        }
     }
 }
