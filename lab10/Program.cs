feaa5a908807fd0dd587d2600eaf891d6e60f1f1
Javier Flores <javier.flores@tecsup.edu.pe>
2021-10-21 21:43:37 -0500

    Laboratorio 10 - Punto 2: a, b, c, d (Configuración de clase LINQ)

---
+++ lab10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        }
+
+    }
+}
