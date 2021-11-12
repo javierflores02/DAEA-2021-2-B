933f7d21b7bd6ee104b9a47010d3567483f52494
Javier Flores <javier.flores@tecsup.edu.pe>
2021-11-12 15:15:00 -0500

    Laboratorio 13: Creación de la vista

--- Lab13/Controllers/PersonaController.cs
+++ Lab13/Controllers/PersonaController.cs
@@ -48,5 +48,43 @@ namespace Lab13.Controllers
             return View(personas);
 
         }
+
+        public ActionResult Mostrar(int id)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergreen 123",
+                FechaNacimiento = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNacimiento = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Los manzamos 101",
+                FechaNacimiento = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@mail.com"
+            });
+
+            Persona persona = (from p in personas
+                               where p.PersonaID == id
+                               select p).FirstOrDefault();
+
+            return View(persona);
+        }
     }
 }
