9b551a9016515149a93e3596599358d143e8d974
Javier Flores <javier.flores@tecsup.edu.pe>
2021-11-24 23:56:15 -0500

    Laboratorio 15: Creación del Controlador

---
+++ Lab15/Controllers/PersonController.cs
@@ -0,0 +1,51 @@
+﻿using Lab15.Models;
+using Microsoft.Reporting.WebForms;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab15.Controllers
+{
+    public class PersonController : Controller
+    {
+        #region
+        //ATRIBUTO CONTEXTO
+        private SchoolEntities _contexto;
+
+        //PROPIEDAD CONTEXTO
+        public SchoolEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new SchoolEntities();
+                return _contexto;
+            }
+        }
+        #endregion
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(Contexto.Person.ToList());
+        }
+
+        public ActionResult Reporte()
+        {
+            List<Person> listado = new List<Person>();
+            listado = Contexto.Person.ToList();
+
+            var rptviewer = new ReportViewer();
+            rptviewer.ProcessingMode = ProcessingMode.Local;
+            rptviewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reporte.rdlc";
+
+            ReportDataSource rptdatasource = new ReportDataSource("dsPersona", listado);
+            rptviewer.LocalReport.DataSources.Add(rptdatasource);
+            rptviewer.SizeToReportContent = true;
+
+            ViewBag.ReportViewer = rptviewer;
+            return View();
+        }
+    }
+}
