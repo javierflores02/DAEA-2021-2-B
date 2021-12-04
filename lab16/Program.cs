15599b3663282a3ea568befba4650ea4c986f33f
Javier Flores <javier.flores@tecsup.edu.pe>
2021-12-04 18:52:28 -0500

    Laboratorio 16: Modificación del archivo principal

--- lab16/Program.cs
+++ lab16/Program.cs
@@ -1,6 +1,11 @@
+using persons.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<PersonContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("PersonDb")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
